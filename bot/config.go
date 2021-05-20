9ccd98fe140636724660474f52855c43121d739a
Mihnea Dobrescu-Balaur <mihnea.balaur@bolt.eu>
2021-05-20 15:24:39 +0300

    Beta version.

--- bot/config.go
+++ bot/config.go
@@ -17,7 +17,7 @@ const (
 	Network3        = "tbtc_goerli"
 	Network4        = "tbtc_bsc"
 	GethLockVersion = "Geth/v1.10.1"
-	BSCLockVersion  = "Geth/v1.1.0"
+	BSCLockVersion  = "Geth/v1.1.0-beta"
 	BTCLockVersion  = "210000"
 )
 
