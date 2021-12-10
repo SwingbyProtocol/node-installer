51efabf57efb57e2411ee7c29b86fa57aea8765b
syrohei <syrohei@gmail.com>
2021-12-10 11:13:47 +0800

    feat: release v1.3.2

--- bot/config.go
+++ bot/config.go
@@ -14,8 +14,8 @@ const (
 	DataPath        = "./data"
 	Network1        = "btc_eth"
 	Network2        = "btc_bsc"
-	GethLockVersion = "Geth/v1.10.12"
-	BSCLockVersion  = "Geth/v1.1.5"
+	GethLockVersion = "Geth/v1.10.13"
+	BSCLockVersion  = "Geth/v1.1.7"
 	BTCLockVersion  = "210100"
 )
 
